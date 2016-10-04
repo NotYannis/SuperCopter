fb6163f33b4d0ef2d4fdebed318c5b5f36c1b745
ATTARD Yannis <Yannis.attard@etudiant.cnam-enjmin.fr>
2016-10-04 17:24:54 +0200

    better game loop and beginning of mbf

--- SuperCopter/Source.cpp
+++ SuperCopter/Source.cpp
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <iostream>
 #include <Windows.h>
+#include "NYTimer.cpp"
+#include <vector>
 
 using namespace std;
 
 const int SCREEN_WIDTH = 80;
 const int SCREEN_HEIGHT = 25;
 const int xShip = 5;
-
+int upLine1[SCREEN_WIDTH];
+int upLine2[SCREEN_WIDTH];
+int upLine3[SCREEN_WIDTH];
+int downLine1[SCREEN_WIDTH];
+int downLine2[SCREEN_WIDTH];
+int downLine3[SCREEN_WIDTH];
+NYTimer timer;
 
 void draw(int yShip, int cpt){
 	HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
@@ -90,18 +98,47 @@ int asciiPhysics(int yPos, int cpt){
 	}
 }
 
+int rand_a_b(int a, int b){
+	return rand() % (b - a) + a;
+}
+
+void mBfUp(vector<int>& up){
+
+}
+
+void init(vector<int>& upL1, vector<int>& upL2, vector<int>& downL1, vector<int>& downL2){
+	upL1[0] = rand_a_b(1, 7);
+	upL1[SCREEN_WIDTH - 1] = rand_a_b(1, 7);
+	upL2[0] = upL1[SCREEN_WIDTH - 1];
+	upL2[SCREEN_WIDTH - 1] = rand_a_b(1, 7);
+	downL1[0] = rand_a_b(17, 24);
+	downL1[SCREEN_WIDTH - 1] = rand_a_b(17, 24);
+	downL2[0] = downL1[SCREEN_WIDTH - 1];
+	downL2[SCREEN_WIDTH - 1] = rand_a_b(17, 24);
+}
+
+
 
 int main(int argc, char * argv[]){
 	bool dead = false;
 	int cpt = 0;
 	int yPos = 8;
+
+
+
+	timer.start();
 	while (!dead){
-		draw(yPos, cpt);
-		yPos = asciiPhysics(yPos, cpt);
-		++cpt;
-		Sleep(100);
-		if (yPos == 6 || yPos == 19){
-			dead = true;
+		if (timer.getElapsedSeconds() > 0.1f){
+			draw(yPos, cpt);
+			yPos = asciiPhysics(yPos, cpt);
+			++cpt;
+			if (yPos == 5 || yPos == 19){
+				dead = true;
+			}
+			
+			LARGE_INTEGER timeNow;
+			QueryPerformanceCounter(&timeNow);
+			timer.lastUpdateTime = timeNow;
 		}
 	}
 	return 0;
